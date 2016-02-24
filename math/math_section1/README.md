e864d69de791db54abfff6d94566a1dcde402230
yusra wahbah <yusrawahbah@yusras-MacBook-Pro.local>
2016-02-24 09:07:43 -0500

    added README and math

--- math/math_section1/README.md
+++
@@ -1,3 +0,0 @@
-# Math program for Demonstrating TDD
-
-This is a short program that does basic arithmetic and demonstrates used of test-driven development (TDD) 
---
+++ spec/math_spec.js
@@ -0,0 +1,5 @@
+describe("A math program that does artimetic", function() {
+  it("adds two integers together", function() {
+    expect(add(3, 5)).toBe(8);
+  });
+});
