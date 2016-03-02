938f9ec5b5c86b940c26c3db52141548569cc5d6
wahbahym <wahbahym@dukes.jmu.edu>
2016-03-02 09:02:50 -0500

    Update math_spec.js

--- spec/math_spec.js
+++ spec/math_spec.js
@@ -1,8 +1,22 @@
 // tell the test where to find the code
 var math = require('../math');
 
-describe("A math program that does artimetic", function() {
-  it("can adds two integers together", function() {
-    expect(math.add(3, 5)).toBe(8);
+describe("A simple program that does arithmetic", function() {
+
+  it("can add two numbers", function() {
+    expect(math.add(3,5)).toBe(8);
   });
+
+  it("can subtract two numbers", function() {
+  	expect(math.subtract(3,5)).toBe(-2);
+  });
+
+  it("can multiply two numbers", function() {
+    expect(math.multiply(3,5)).toBe(15);
+  });
+
+  it("can divide two numbers", function() {
+  	expect(math.divide(15,5)).toBe(3);
+  });
+  
 });
